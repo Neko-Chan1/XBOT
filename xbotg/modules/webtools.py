6f89966221bae308261e204795026e865e6f30a7
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-01 06:33:33 +0700

    Add files via upload

---
+++ xbotg/modules/webtools.py
@@ -0,0 +1,132 @@
+# UserindoBot
+# Copyright (C) 2020  UserindoBot Team, <https://github.com/MoveAngel/UserIndoBot.git>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import datetime
+import html
+import os
+import platform
+import subprocess
+import time
+import speedtest
+from platform import python_version
+
+import requests
+from psutil import boot_time, cpu_percent, disk_usage, virtual_memory
+from spamwatch import __version__ as __sw__
+from telegram import ParseMode, __version__
+from telegram.error import BadRequest
+from telegram.ext import CommandHandler, Filters, run_async
+
+from xbotg import MESSAGE_DUMP, OWNER_ID, dispatcher
+from xbotg.modules.helper_funcs.filters import CustomFilters
+
+
+@run_async
+def get_bot_ip(update, context):
+    """Sends the bot's IP address, so as to be able to ssh in if necessary.
+    OWNER ONLY.
+    """
+    res = requests.get("http://ipinfo.io/ip")
+    update.message.reply_text(res.text)
+
+
+@run_async
+def system_status(update, context):
+    uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+    status = "<b>======[ SYSTEM INFO ]======</b>\n\n"
+    status += "<b>System uptime:</b> <code>" + str(uptime) + "</code>\n"
+
+    uname = platform.uname()
+    status += "<b>System:</b> <code>" + str(uname.system) + "</code>\n"
+    status += "<b>Node name:</b> <code>" + str(uname.node) + "</code>\n"
+    status += "<b>Release:</b> <code>" + str(uname.release) + "</code>\n"
+    status += "<b>Version:</b> <code>" + str(uname.version) + "</code>\n"
+    status += "<b>Machine:</b> <code>" + str(uname.machine) + "</code>\n"
+    status += "<b>Processor:</b> <code>" + str(uname.processor) + "</code>\n\n"
+
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
+    status += "<b>CPU usage:</b> <code>" + str(cpu) + " %</code>\n"
+    status += "<b>Ram usage:</b> <code>" + str(mem[2]) + " %</code>\n"
+    status += "<b>Storage used:</b> <code>" + str(disk[3]) + " %</code>\n\n"
+    status += "<b>Python version:</b> <code>" + python_version() + "</code>\n"
+    status += "<b>Library version:</b> <code>" + str(__version__) + "</code>\n"
+    status += "<b>Spamwatch API:</b> <code>" + str(__sw__) + "</code>\n"
+    context.bot.sendMessage(update.effective_chat.id, status, parse_mode=ParseMode.HTML)
+
+
+def speed_convert(size):
+    """Hi human, you can't read bytes?"""
+    power = 2 ** 10
+    zero = 0
+    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+
+@run_async
+def gitpull(update, context):
+    sent_msg = update.effective_message.reply_text("Pulling all changes from remote...")
+    subprocess.Popen("git pull", stdout=subprocess.PIPE, shell=True)
+
+    sent_msg_text = (
+        sent_msg.text
+        + "\n\nChanges pulled... I guess..\nContinue to restart with /reboot "
+    )
+    sent_msg.edit_text(sent_msg_text)
+
+
+@run_async
+def restart(update, context):
+    user = update.effective_message.from_user
+
+    update.effective_message.reply_text(
+        "Starting a new instance and shutting down this one"
+    )
+
+    if MESSAGE_DUMP:
+        datetime_fmt = "%H:%M - %d-%m-%Y"
+        current_time = datetime.datetime.utcnow().strftime(datetime_fmt)
+        message = (
+            f"<b>Bot Restarted </b>"
+            f"<b>By :</b> <code>{html.escape(user.first_name)}</code>"
+            f"<b>\nDate Bot Restart : </b><code>{current_time}</code>"
+        )
+        context.bot.send_message(
+            chat_id=MESSAGE_DUMP,
+            text=message,
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True,
+        )
+
+    os.system("bash start")
+
+
+IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
+SYS_STATUS_HANDLER = CommandHandler(
+    "sysinfo", system_status, filters=CustomFilters.dev_filter
+)
+
+GITPULL_HANDLER = CommandHandler("gitpull", gitpull, filters=CustomFilters.dev_filter)
+RESTART_HANDLER = CommandHandler("reboot", restart, filters=CustomFilters.dev_filter)
+
+dispatcher.add_handler(IP_HANDLER)
+dispatcher.add_handler(SYS_STATUS_HANDLER)
+dispatcher.add_handler(GITPULL_HANDLER)
+dispatcher.add_handler(RESTART_HANDLER)
