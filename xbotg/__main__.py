e9a0cfe011c1252ce3bdd085aeb2d5b397a24b36
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-01 06:45:45 +0700

    Update __main__.py

--- xbotg/__main__.py
+++ xbotg/__main__.py
@@ -497,7 +497,7 @@ def main():
             updater.bot.set_webhook(url=URL + TOKEN)
 
     else:
-        LOGGER.info("Using long polling.")
+        LOGGER.info("Telah Aktif 🔥")
         updater.start_polling(timeout=15, read_latency=4)
     if len(argv) not in (1, 3, 4):
         telethn.disconnect()
