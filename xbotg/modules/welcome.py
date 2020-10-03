6db784c641e34180465d5cc8377471e67aa9ec02
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-03 23:09:02 +0700

    Update welcome.py

--- xbotg/modules/welcome.py
+++ xbotg/modules/welcome.py
@@ -145,7 +145,7 @@ def new_member(bot: Bot, update: Update, job_queue: JobQueue):
 
             # Welcome yourself
             elif new_mem.id == bot.id:
-                update.effective_message.reply_text("Watashi ga kitta!")
+                update.effective_message.reply_text("Thanks to added your group 🔥..")
 
             else:
                 # If welcome message is media, send with appropriate function
