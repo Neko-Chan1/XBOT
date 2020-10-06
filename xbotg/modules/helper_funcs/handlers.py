00be51a9907212baf389e2403a7ed8814d16d0a6
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-06 19:16:37 +0700

    Update handlers.py

--- xbotg/modules/helper_funcs/handlers.py
+++ xbotg/modules/helper_funcs/handlers.py
@@ -18,9 +18,9 @@ class CustomCommandHandler(CommandHandler):
             del kwargs["admin_ok"]
         super().__init__(command, callback, **kwargs)
 
-    def check_update(self, update):
-
-        if isinstance(update, Update) and (update.message or update.edited_message and self.allow_edited):
+        def check_update(self, update):
+        if (isinstance(update, Update)
+                and (update.message or update.edited_message and self.allow_edited)):
             message = update.message or update.edited_message
 
             if sql.is_user_blacklisted(update.effective_user.id):
@@ -28,11 +28,9 @@ class CustomCommandHandler(CommandHandler):
 
             if message.text and len(message.text) > 1:
                 fst_word = message.text_html.split(None, 1)[0]
-
                 if len(fst_word) > 1 and any(fst_word.startswith(start) for start in CMD_STARTERS):
                     command = fst_word[1:].split('@')
                     command.append(message.bot.username)  # in case the command was sent without a username
-
                     if self.filters is None:
                         res = True
                     elif isinstance(self.filters, list):
@@ -54,3 +52,27 @@ class CustomRegexHandler(RegexHandler):
 class CustomMessageHandler(MessageHandler):
     def __init__(self, filters, callback, _friendly="", **kwargs):
         super().__init__(filters, callback, **kwargs)
+
+class GbanLockHandler(tg.CommandHandler):
+    def __init__(self, command, callback, **kwargs):
+        super().__init__(command, callback, **kwargs)
+    def check_update(self, update):
+        if (isinstance(update, Update) and (update.message or update.edited_message and self.allow_edited)):
+            message = update.message or update.edited_message
+            if sql.is_user_gbanned(update.effective_user.id):
+                return False
+            if message.text and message.text.startswith('/') and len(message.text) > 1:
+                first_word = message.text_html.split(None, 1)[0]
+                if len(first_word) > 1 and first_word.startswith('/'):
+                    command = first_word[1:].split('@')
+                    command.append(message.bot.username)  # in case the command was sent without a username
+                    if not (command[0].lower() in self.command and command[1].lower() == message.bot.username.lower()):
+                        return False
+                    if self.filters is None:
+                        res = True
+                    elif isinstance(self.filters, list):
+                        res = any(func(message) for func in self.filters)
+                    else:
+                        res = self.filters(message)
+                    return res
+            return False
