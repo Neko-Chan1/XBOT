6ce39cfa9dff5b24f860a2f455cb6869325cd327
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-10 18:40:31 +0700

    Update magictext.py

--- xbotg/modules/magictext.py
+++ xbotg/modules/magictext.py
@@ -1,42 +1,49 @@
-from typing import List
-
-from telegram import Bot, Update
-from telegram.ext import run_async
-
 from xbotg import dispatcher
 from xbotg.modules.disable import DisableAbleCommandHandler
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
 
-normiefont = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-              'v', 'w', 'x', 'y', 'z']
-weebyfont = ['🅐', '🅑', '🅒', '🅓', '🅔', '🅕', '🅖', '🅗', '🅘', '🅙', '🅚', '🅛', '🅜', '🅝', '🅞', '🅟', '🅠', '🅡', '🅢', '🅣', '🅤',
-              '🅥', '🅦', '🅧', '🅨', '🅩']
+normiefont = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+weebyfont = [
+    '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁', '长', '乚', '从', '𠘨', '口',
+    '尸', '㔿', '尺', '丂', '丅', '凵', 'リ', '山', '乂', '丫', '乙'
+]
 
 
 @run_async
-def weebify(bot: Bot, update: Update, args: List[str]):
-    string = '  '.join(args).lower()
+def weebify(update: Update, context: CallbackContext):
+    args = context.args
+    message = update.effective_message
+    string = ""
+
+    if message.reply_to_message:
+        string = message.reply_to_message.text.lower().replace(" ", "  ")
+
+    if args:
+        string = '  '.join(args).lower()
+
+    if not string:
+        message.reply_text(
+            "Usage is `/weebify <text>`", parse_mode=ParseMode.MARKDOWN)
+        return
+
     for normiecharacter in string:
         if normiecharacter in normiefont:
             weebycharacter = weebyfont[normiefont.index(normiecharacter)]
             string = string.replace(normiecharacter, weebycharacter)
 
-    message = update.effective_message
     if message.reply_to_message:
         message.reply_to_message.reply_text(string)
     else:
         message.reply_text(string)
 
 
-__help__ = """
- Originally Made By [Ayan Ansari](t.me/TechnoAyanOfficial)
- 
- - /blackout <text>: Apply Blackout Style to your text
- """
-
-WEEBIFY_HANDLER = DisableAbleCommandHandler("blackout", weebify, pass_args=True)
+WEEBIFY_HANDLER = DisableAbleCommandHandler("weebify", weebify)
 
 dispatcher.add_handler(WEEBIFY_HANDLER)
 
-__mod_name__ = "Black Out"
-command_list = ["weebify"]
-handlers = [WEEBIFY_HANDLER]
+__command_list__ = ["weebify"]
+__handlers__ = [WEEBIFY_HANDLER]
