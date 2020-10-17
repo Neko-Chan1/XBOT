752b0ae53917d2017d23f54461ce55aadfc3abef
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-17 18:08:48 +0700

    Update global_bans.py

--- xbotg/modules/global_bans.py
+++ xbotg/modules/global_bans.py
@@ -20,8 +20,8 @@ from xbotg import (
     WHITELIST_USERS,
     STRICT_GBAN,
     GBAN_LOGS,
-    SW_API,
 )
+from xbotg import SW_API as sw
 from xbotg.modules.helper_funcs.chat_status import (
     user_admin,
     is_user_admin,
@@ -508,10 +508,10 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 /gbanstat
 Note: You can appeal gbans or ask gbans at @YorktownEagleUnion
 
-Kigyo also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
+XBOT also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/ac12a2c6b831dd005015b.jpg)
-Kigyo will constantly help banning spammers off from your group automatically So, you don't have to worry about spammers storming your group.
+XBOT will constantly help banning spammers off from your group automatically So, you don't have to worry about spammers storming your group.
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban, pass_args=True)
