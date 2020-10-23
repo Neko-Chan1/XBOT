c7c6073620f00dd6c2b4b3a524062b968525dc0f
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-10-23 13:32:17 +0700

    Update wiki.py

--- xbotg/modules/wiki.py
+++ xbotg/modules/wiki.py
@@ -1,47 +1,19 @@
-import random
-import re
-import json
-import urllib.request
-import urllib.parse
-import wikipedia
-from wikipedia.exceptions import DisambiguationError, PageError
-
-
-from telegram import Message, Chat, Update, Bot, ParseMode
-from telegram.ext import run_async
-
+from typing import Optional, List
+from telegram import Update, Bot
 from xbotg import dispatcher
 from xbotg.modules.disable import DisableAbleCommandHandler
+import wikipedia
 
 
-@run_async
-def wiki(bot: Bot, update: Update, args):
-    msg = update.effective_message.reply_to_message if update.effective_message.reply_to_message else update.effective_message
-    res = ""
-    if msg == update.effective_message:
-        search = msg.text.split(" ", maxsplit=1)[1]
-    else:
-        search = msg.text
-    try:
-        res = wikipedia.summary(search)
-    except DisambiguationError as e:
-        update.message.reply_text("Disambiguated pages found! Adjust your query accordingly.\n<i>{}</i>".format(e),
-        parse_mode=ParseMode.HTML)
-    except PageError as e:
-        update.message.reply_text("<code>{}</code>".format(e), parse_mode=ParseMode.HTML)
-    if res:
-        result = f"<b>{search}</b>\n\n"
-        result += f"<i>{res}</i>\n"
-        result += f"""<a href="https://en.wikipedia.org/wiki/{search.replace(" ", "%20")}">Read more...</a>"""
-        if len(result) > 4000:
-            with open("result.txt", 'w') as f:
-                f.write(f"{result}\n\nUwU OwO OmO UmU")
-            with open("result.txt", 'rb') as f:
-                bot.send_document(document=f, filename=f.name,
-                    reply_to_message_id=update.message.message_id, chat_id=update.effective_chat.id,
-                    parse_mode=ParseMode.HTML)
-        else:
-            update.message.reply_text(result, parse_mode=ParseMode.HTML)
+def wiki(_bot: Bot, update: Update, args):
+    reply = " ".join(args)
+    summary = '{} {}'
+    update.message.reply_text(
+        summary.format(
+            wikipedia.summary(
+                reply,
+                sentences=3),
+            wikipedia.page(reply).url))
 
 
 __help__ = """
